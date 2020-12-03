4e1d74227460b65a45eccb54fa61a776bc43db62
tkm_mkzk <t.m.08013440879@icloud.com>
2020-12-03 18:49:45 +0900

    rubocop実施

--- spec/factories/blogs.rb
+++ spec/factories/blogs.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :blog do
-    title       { 'エニタイムフィットネス千歳烏山店でのトレーニング'}
-    target_site { '腕、胸'}
+    title       { 'エニタイムフィットネス千歳烏山店でのトレーニング' }
+    target_site { '腕、胸' }
     content     { '今日は胸周りの筋肉を中心にトレーニングを実施。ベンチプレスやインクラインダンベルプレス、ケーブルプレス、アームカールを行った。' }
-    association        :user
+    association :user
 
     after(:build) do |blog|
       blog.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
