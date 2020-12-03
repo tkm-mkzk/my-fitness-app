146df1181ba1b9b2a88d5341d3502e3a1e547901
tkm_mkzk <t.m.08013440879@icloud.com>
2020-12-03 18:04:43 +0900

    日記投稿機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :blogs, dependent: :destroy
+
   validates :nickname, presence: true
 
   devise :database_authenticatable, :registerable,
