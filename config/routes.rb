8232bf974a31f94cf1db255652322a12bba7f768
tkm_mkzk <t.m.08013440879@icloud.com>
2020-12-04 16:06:15 +0900

    ユーザー詳細機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root 'blogs#index'
   resources :blogs do
     resources :comments, only: :create
   end
+  resources :users, only: [:show, :edit, :update]
 end
