6c2f1613529d549401aa70ea189a32de9ca6e492
tkm_mkzk <t.m.08013440879@icloud.com>
2020-12-10 16:48:28 +0900

    未ログイン者の遷移先の設定

--- app/controllers/body_weights_controller.rb
+++ app/controllers/body_weights_controller.rb
@@ -1,5 +1,5 @@
 class BodyWeightsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :update, :destroy]
+  before_action :authenticate_user!
   before_action :set_user
   before_action :allow_correct_user, only: [:create, :update, :destroy]
 
