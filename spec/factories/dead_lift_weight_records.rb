e23f348a9e4136d26875e48fc6878b8a3a0810e1
tkm_mkzk <t.m.08013440879@icloud.com>
2020-12-12 14:37:02 +0900

    rubocopの実施

--- spec/factories/dead_lift_weight_records.rb
+++ spec/factories/dead_lift_weight_records.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
   factory :dead_lift_weight_record do
-    
   end
 end
--- spec/requests/dead_lift_weight_records_request_spec.rb
+++ spec/requests/dead_lift_weight_records_request_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe "DeadLiftWeightRecords", type: :request do
-
+RSpec.describe 'DeadLiftWeightRecords', type: :request do
 end
