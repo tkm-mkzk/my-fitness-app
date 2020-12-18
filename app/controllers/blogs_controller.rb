9bee7647a597bd9cb792de3c9bebc6276135d0d0
tkm_mkzk <t.m.08013440879@icloud.com>
2020-12-18 15:24:14 +0900

    viewの調整

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   before_action :move_to_index, only: :edit
 
   def index
-    @blogs = Blog.all.page(params[:page]).order('created_at DESC')
+    @blogs = Blog.all.page(params[:page]).per(12).order('created_at DESC')
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,36 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @blogs = @user.blogs
+    @blogs = @user.blogs.order(updated_at: :desc).page(params[:page]).per(3)
+    @current_body_weight = @user.body_weights.order(updated_at: :desc).limit(1)
+    @current_bench_press_weight = @user.bench_press_weight_records.order(updated_at: :desc).limit(1)
+    @current_dead_lift_weight = @user.dead_lift_weight_records.order(updated_at: :desc).limit(1)
+    @current_squat_weight = @user.squat_weight_records.order(updated_at: :desc).limit(1)
+    @body_weights = @user.body_weights.all
+    @bench_press_weight_records = @user.bench_press_weight_records.all
+    @dead_lift_weight_records = @user.dead_lift_weight_records.all
+    @squat_weight_records = @user.squat_weight_records.all
+
+    if @current_bench_press_weight[0] == nil
+    a = 0
+      else
+    a = @current_bench_press_weight[0].bench_press_weight
+    end
+
+    if @current_squat_weight[0]  == nil
+    b = 0
+      else
+    b = @current_squat_weight[0].squat_weight
+    end
+
+    if @current_dead_lift_weight[0] == nil
+    c = 0
+      else
+    c = @current_dead_lift_weight[0].dead_lift_weight
+    end
+
+    @total_big3 = a + b + c
+
   end
 
   def edit
