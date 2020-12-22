07fa950de22d96df103ece7562d6965a2041f0f9
tkm_mkzk <t.m.08013440879@icloud.com>
2020-12-22 12:28:23 +0900

    ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    sessions: 'users/sessions'
   }
 
   devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy"
+    get "sign_in", to: "users/sessions#new"
+    get "sign_out", to: "users/sessions#destroy"
     get 'users', to: 'users/registrations#new'
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
 
   root 'blogs#index'
