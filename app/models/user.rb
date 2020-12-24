0134da2f7b976ce134afa35824d52adcc52023c1
tkm_mkzk <t.m.08013440879@icloud.com>
2020-12-24 17:13:18 +0900

    privateバリデーションの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :private, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Password Include both letters and numbers' }, on: :create
 
   def self.guest
