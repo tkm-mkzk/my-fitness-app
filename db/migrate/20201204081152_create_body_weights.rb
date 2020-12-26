36d6f20e82d6990cecf344784315214fcd7f9faa
tkm_mkzk <t.m.08013440879@icloud.com>
2020-12-26 17:25:51 +0900

    integerをfloatに変更

--- db/migrate/20201204081152_create_body_weights.rb
+++ db/migrate/20201204081152_create_body_weights.rb
@@ -1,7 +1,7 @@
 class CreateBodyWeights < ActiveRecord::Migration[6.0]
   def change
     create_table :body_weights do |t|
-      t.integer      :weight
+      t.float      :weight
       t.date       :day
       t.references :user,   foreign_key: true
       t.timestamps
